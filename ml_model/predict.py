8c429e7f4bcccf7262ae9119cae6274fac3d7c2e
sevggii <sevgitr_@hotmail.com>
2025-07-02 16:49:20 +0300

    covid tespiti deneme testi

--- ml_model/predict.py
+++ ml_model/predict.py
@@ -8,23 +8,42 @@ semptomlar = [
     "İshal", "Koku veya Tat Kaybı", "Nefes Darlığı", "Öksürük", "Vücut Ağrıları"
 ]
 
+'''
+  "yaygin": 1.0,
+    "az_yaygin": 0.75,
+    "yaygin_olmayan": 0.5,
+    "nadir": 0.25,
+    "gorulmez": 0.0
+'''
+
 # Kullanıcının girişine göre (1 = var, 0 = yok)
 # Bu kısmı kendin test edebilirsin
 kullanici_semptomlari = {
     "Ateş": 1,
-    "Baş Ağrısı": 0,
+    "Baş Ağrısı": 0.75,
     "Bitkinlik": 1,
-    "Boğaz Ağrısı": 1,
-    "Bulantı veya Kusma": 0,
-    "Burun Akıntısı veya Tıkanıklığı": 0,
-    "Göz Kaşıntısı veya Sulanma": 0,
-    "Hapşırma": 0,
-    "İshal": 0,
-    "Koku veya Tat Kaybı": 1,
+    "Boğaz Ağrısı": 0.75,
+    "Bulantı veya Kusma": 0.5,
+    "Burun Akıntısı veya Tıkanıklığı": 0.25,
+    "Göz Kaşıntısı veya Sulanma": 0.25,
+    "Hapşırma": 0.25,
+    "İshal": 0.5,
+    "Koku veya Tat Kaybı": 0.75,
     "Nefes Darlığı": 1,
     "Öksürük": 1,
-    "Vücut Ağrıları": 1
+    "Vücut Ağrıları": 0.75
 }
+'''
+🩺 Tahmin Edilen Hastalık: COVID-19
+
+📊 Olasılıklar:
+  COVID-19: %100.0
+  Grip: %0.0
+  Mevsimsel Alerji: %0.0
+  Soğuk Algınlığı: %0.0
+'''
+
+
 
 # Belirti vektörünü sıraya göre oluştur
 input_vector = [kullanici_semptomlari.get(s, 0) for s in semptomlar]
